215b8dea6b56222d5be60f762104cb7ebaf63448
Antra Gupta <antra1947@gmail.com>
2026-04-13 18:25:10 +0530

    fix: relax profile validator, allow empty emergency contact fields

--- backend/src/modules/user/userValidator.js
+++ backend/src/modules/user/userValidator.js
@@ -1,18 +1,18 @@
 const Joi = require("joi");
 
 const updateProfileSchema = Joi.object({
-  firstName: Joi.string().optional(),
-  lastName:  Joi.string().optional(),
-  photoUrl:  Joi.string().uri().optional().allow(""),
+  firstName: Joi.string().optional().allow(""),
+  lastName:  Joi.string().optional().allow(""),
+  photoUrl:  Joi.string().optional().allow(""),
   safetyProfile: Joi.object({
     bloodGroup:   Joi.string().optional().allow(""),
     allergies:    Joi.string().optional().allow(""),
     medicalNotes: Joi.string().optional().allow(""),
     emergencyContacts: Joi.array().items(
       Joi.object({
-        name:  Joi.string().required(),
-        phone: Joi.string().required(),
-        email: Joi.string().email().optional().allow(""),
+        name:  Joi.string().optional().allow(""),
+        phone: Joi.string().optional().allow(""),
+        email: Joi.string().optional().allow(""),
       })
     ).optional(),
   }).optional(),
